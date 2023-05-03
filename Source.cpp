3012cf45c81346b16ffc2f412356b2c9b5e35d06
Abdul Rehman <129746474+Abdul-Rehman-13812@users.noreply.github.com>
2023-05-03 22:11:22 +0500

    link list practice
    link list + delete at first

--- Source.cpp
+++ Source.cpp
@@ -1,125 +1,90 @@
 #include<iostream>
 using namespace std;
-char*deepCopy(char*p)
+struct node
 {
-	int length = strlen(p);
-	char*temp = new char[length + 1];
-	for (int i = 0; i < length; i++)
-	{
-		temp[i] = p[i];
-	}
-	temp[length] = '\0';
-	return temp;
-}
-
-class list
-{
-public:
-	char* arr;
-	int size;
-	int top = -1;
-	list()
-	{
-		arr = nullptr;
-		size = 0;
-	}
-	list(char *a, int s)
-	{
-		arr = a;
-		size = s;
-		top = -1;
-	}
-	list(const list& obj)
-	{
-		arr = obj.arr;
-		size = obj.size;
-	}
-	void push(char value)
-	{
-		if (isfull() == true)
-		{
-			cout << "underflow:" << endl;
-		}
-		top++;
-		arr[top] = value;
-		//	cout << arr << endl;
-
-	}
-
-
-	void display()
-	{
-		for (int i = 0; i <= top; i++)
-		{
-			cout << arr[i] << "  " << endl;
-		}
-		cout << "sizeo of stack is:" << size1();
-
-	}
-	char pop()
-	{
-		char tem = arr[top];
-		top--;
-		cout << "pop:" << tem << endl;
-		return tem;
-	}
-	bool isepmty()
-	{
-		if (top == -1)
-		{
-			cout << "stack is empty:" << endl;
-			return true;
-		}
-
-	}
-	bool isfull()
-	{
-		if (top == size - 1)
-		{
-			cout << "stack is full:" << endl;
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-
-	}
-	int size1()
-	{
-		int ss = top + 1;
-		return ss;
-	}
-	/*~list()
-	{
-	delete[] arr;
-	}*/
+	int data;
+	node* next;
 };
+	class list
+	{
+	private:
+		node*first, *tail;
+	public:
+		list()
+		{
+			first = tail = nullptr;
+		}
+		void add(int x)
+		{
+			node *ptr = new node;
+			ptr->data = x;
+			ptr->next = nullptr;
+			
+			if (first == nullptr)
+			{
+				first = tail = ptr;
+			}
+			else
+			{
+				tail->next = ptr;
+				tail = ptr;
+			}
 
 
-int main()
-{
-	char arr[3];
-	list obj(arr,3);
-	obj.push('A');
-	obj.push('b');
-	obj.push('r');
+		}
+
+		void show()
+		{
+			node *p = first;
+			while (p!= nullptr)
+			{
+				{
+					cout << p->data << endl;
+					/*
+					if (p->data % 10 == 0)
+					{
+						cout << p->data << endl;
+						
+					}*/
+					p = p->next;
+				}
+				
+				
+			}
+			cout << endl;
+			
+		}
+		int delete_first()
+		{
+			if (first == nullptr)
+			{
+				cout << "under flow:" << endl;
+			}
+			else
+			{
+				node* tem = first;
+				first = first->next;
+				delete tem;
+				
+			}
+			return 0;
+		}
+
+
+
+	};
 	
-	/*obj.push('c');
-	obj.push('c');
-	obj.push('c');
-	obj.push('c');*/
-	//obj.display();
-//	obj.pop();
-	//obj.pop();
-	//obj.pop();
-	//bj.pop();
-	obj.size1();
-	obj.display();
-	obj.isepmty();
-	obj.isfull();
- //  obj.display();
 
-	return 0;
+void main()
+{
+	list l1;
+	//l1.show();
+	l1.add(10);
+	//l1.show();
+	l1.add(20);
+	l1.show();
+	l1.delete_first();
+	l1.show();
+
 
 }
